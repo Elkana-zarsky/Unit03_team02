9dd491524cf172aed14a195a088d3e1f233f75f7
Elkana-zarsky <thnubhfuar@gmail.com>
2025-12-23 17:26:11 +0200

    Update method 4 shiftLeft

--- Unit03.java
+++ Unit03.java
@@ -6,9 +6,17 @@ public class Unit03{
     public static int shiftRight(int num, int k){
         return -1;
     }
-   
-    public static int shiftLeft(int num)    {
-        return -1;
+       public static int shiftLeft(int num){
+        if(num<=999 || num>=10000){
+            num = -1;
+        }
+        else{
+            int firstDigit;
+            firstDigit = num / 1000;
+            num = (num-1000*firstDigit)*10;
+            num = num + firstDigit;
+        }
+        return num;
     }
     public static int shiftLeft(int num, int k){
         return -1;
